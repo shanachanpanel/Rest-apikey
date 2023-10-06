b139f368cf10d0b165d8efe4624ce2c50b4afc1a
shanachan xixixi <126304667+shanachanpanel@users.noreply.github.com>
2023-10-06 13:30:34 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+global.creator = 'shanachan'// Creator
+global.MONGO_DB_URI = "mongodb+srv://danzzapiuserdata:Danzz804505@cluster0.j3kozll.mongodb.net/?retryWrites=true&w=majority" // Database
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10"
+global.your_email = "rizaedtz@gmail.com" // Email
+global.email_password = "ttyyxwlaapgockvk" // SMTP Password
+global.APIKEY_PREM = 'shanachan'
+global.limitCount = 10000
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
